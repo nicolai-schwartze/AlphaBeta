26f6f81e776007eb8d226f6ac5b81e33310de6ce
Nicolai Schwartze <nicolai.schwartze@outlook.com>
2025-04-01 00:09:05 +0200

    initialize ocr module

--- python_prototype/ocr_checker.py
+++ python_prototype/ocr_checker.py
@@ -9,23 +9,27 @@ Created on Sat Mar 22 14:46:52 2025
 from transformers import TrOCRProcessor, VisionEncoderDecoderModel
 from PIL import Image
 
-# load processor and model from huggingface
-processor = TrOCRProcessor.from_pretrained('openthaigpt/thai-trocr',)
-model = VisionEncoderDecoderModel.from_pretrained('openthaigpt/thai-trocr')
 
-# get test image
-path = './test_consonant.png'
-solution = 'ก'
-image = Image.open(path).convert("RGB")
 
-# process and generate text
-pixel_values = processor(images=image, return_tensors="pt").pixel_values
-generated_ids = model.generate(pixel_values)
-generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
-print(generated_text)
 
-# compare to solution
-if (generated_text == solution):
-    print('Sucess!')
-else: 
-    print("Try Again")
+if __name__ == "__main__":
+    # load processor and model from huggingface
+    processor = TrOCRProcessor.from_pretrained('openthaigpt/thai-trocr',)
+    model = VisionEncoderDecoderModel.from_pretrained('openthaigpt/thai-trocr')
+    
+    # get test image
+    path = './test_consonant.png'
+    solution = 'ก'
+    image = Image.open(path).convert("RGB")
+    
+    # process and generate text
+    pixel_values = processor(images=image, return_tensors="pt").pixel_values
+    generated_ids = model.generate(pixel_values)
+    generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+    print(generated_text)
+    
+    # compare to solution
+    if (generated_text == solution):
+        print('Sucess!')
+    else: 
+        print("Try Again")
